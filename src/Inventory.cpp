3eae50e401c4c8bf54d0c646554dc25cedd7bd49
Damian Lukasiewicz <damian.lukasiewicz99@gmail.com>
2021-05-21 16:12:49 +0200

    add Inventory

--- src/Inventory.cpp
+++ src/Inventory.cpp
@@ -1,3 +1,46 @@
 #include "Inventory.h"
 
+Item::Item(std::string Name, unsigned int Cost)
+{
+    this->name = Name;
+    this->cost = Cost;
+}
+
+unsigned int Item::getCost(){ return cost; }
+
+std::string Item::getName(){ return name; }
+
+
+
+Inventory::Inventory(unsigned money)
+{
+    this->money = money;
+}
+
+void Inventory::changeGlobalPrices(int change)
+{
+    this->GlobalPrices += change;
+}
+
+int Inventory::getGlobalPrices()
+{
+    return this->GlobalPrices;
+}
+
+void Inventory::changeMoney(int change)
+{
+    if (change<0 && change*-1 >= this->money)  //checks if money + change <= 0
+    {
+        this->money = 0;
+    }
+    else this->money += change;
+
+}
+
+std::vector<Item>* Inventory::getItems()
+{
+    return &items;
+}
+
+int Inventory::GlobalPrices = 0;
 
--- src/Inventory.h
+++ src/Inventory.h
@@ -1,14 +1,43 @@
 #ifndef MARKETSIMULATION_INVENTORY_H
 #define MARKETSIMULATION_INVENTORY_H
 
+#include <string>
+#include <vector>
+
 class Item
 {
+public:
+    Item(std::string, unsigned);
 
+    unsigned getCost();
+
+    std::string getName();
+
+private:
+    std::string name;
+
+    unsigned cost;
 };
 
 class Inventory
 {
+public:
+    Inventory(unsigned );
 
+    int getGlobalPrices();
+
+    void changeGlobalPrices(int);
+
+    void changeMoney(int x);
+
+    std::vector<Item>* getItems();
+
+private:
+    static int GlobalPrices;
+
+    unsigned money;
+
+    std::vector<Item> items;
 };
 
 
